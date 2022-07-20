9698777b7c0b6881bbc68a443801299e17bef11a
mhtbansal11 <mhtbansal11@gmail.com>
2022-07-20 20:51:56 +0530

    navbar

---
+++ components/navbar.js
@@ -0,0 +1,119 @@
+function navbar(){
+    return `<div id="navbar_container">
+    <div id="logo">
+        <img src="https://www.bing.com/th?id=ODLS.5a16aeae-5032-48af-9d2d-6c0feed3e8a3&w=32&h=32&o=6&pid=13.1"
+            alt="company logo" />
+        <h1><a href="homepage.html">Mentimeter</a></h1>
+    </div>
+    <div id="menu1">
+        <nav>
+            <div class="wrapper">
+                <ul class="nav-links">
+                    <li>
+                        <p>Features
+                            <span class="fas fa-caret-down"></span>
+                        </p>
+                        <ul class="drop_menu1">
+                            <li>
+                                <a href="#">Overview</a>
+                            </li>
+                            <li>
+                                <a href="#">Live Polling</a>
+                            </li>
+                            <li>
+                                <a href="#">Word Cloud</a>
+                            </li>
+                            <li>
+                                <a href="#">Quiz</a>
+                            </li>
+                            <li>
+                                <a href="#">Q&A</a>
+                            </li>
+                            <li>
+                                <a href="#">Survey</a>
+                            </li>
+                            <li>
+                                <a href="#">Presentations</a>
+                            </li>
+                        </ul>
+                    </li>
+    
+                    <li>
+                        <p>Solutions
+                            <span class="fas fa-caret-down"></span>
+                        </p>
+                        <ul class="drop_menu1">
+                            <li>
+                                <a href="#">Overview</a>
+                            </li>
+                            <li>
+                                <a href="#">Speakers</a>
+                            </li>
+                            <li>
+                                <a href="#">Business</a>
+                            </li>
+                            <li>
+                                <a href="#">Education</a>
+                            </li>
+                            <li>
+                                <a href="#">Private events</a>
+                            </li>
+                        </ul>
+                    </li>
+    
+                    <li>
+                        <p>Resources
+                            <span class="fas fa-caret-down"></span>
+                        </p>
+                        <ul class="drop_menu1">
+                            <li>
+                                <a href="#">How to</a>
+                            </li>
+                            <li>
+                                <a href="#">MentiAcademy</a>
+                            </li>
+                            <li>
+                                <a href="#">Templates</a>
+                            </li>
+                            <li>
+                                <a href="#">Webinars</a>
+                            </li>
+                            <li>
+                                <a href="#">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#">Integrations</a>
+                            </li>
+                            <li>
+                                <a href="#">Help Center</a>
+                            </li>
+                        </ul>
+                    </li>
+    
+                    <li>
+                        <a href="#">Pricing</a>
+                    </li>
+    
+                    <li>
+                        <a href="#">Enterprise</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    
+    
+    <div id="menu2">
+        
+            <p>
+                <a href="#" style="text-decoration: none" id="sgn">Sign In</a>
+            </p>
+            <p>
+                <a href="#">Sign Up</a>
+            </p>
+        
+       
+    </div>
+</div>`
+}
+export default navbar ;
