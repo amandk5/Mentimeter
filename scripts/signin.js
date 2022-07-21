50af7325178d0c1720dbfbb62d7a4916ca0e759b
mhtbansal11 <mhtbansal11@gmail.com>
2022-07-22 01:10:49 +0530

    signin commit

---
+++ scripts/signin.js
@@ -0,0 +1,28 @@
+let signinArr = JSON.parse(localStorage.getItem("signupKey")) || [];
+document.querySelector("form").addEventListener("submit", myFunction);
+
+console.log(signinArr);
+
+function myFunction() {
+  event.preventDefault();
+  let EmailAdd = document.querySelector("#email").value;
+  let Pass = document.querySelector("#password").value;
+
+  let flag = false;
+  signinArr.forEach(function (el) {
+    if (EmailAdd == el.oEmail && Pass == el.oPassword) {
+      return (flag = true);
+    }
+  });
+  console.log(flag);
+
+  if (flag) {
+    alert("You Are Successfully Logged In.");
+    localStorage.setItem("system","online");
+    window.location.href = "index.html";
+  } else {
+    alert("Wrong ID/Password");
+
+    window.location.reload();
+  }
+}
